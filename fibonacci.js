2b28a0d9b9d2090ca6a343f61862d6f14e8ead21
pan-ti <pandeandromeda@gmail.com>
2024-10-01 17:21:13 -0300

    corrigido erro de aspas no console.log

--- fibonacci.js
+++ fibonacci.js
@@ -5,3 +5,10 @@ function pertenceASequenciaFibonacci(numero) {
         [anterior, atual] = [atual, anterior + atual];
 } return anterior === numero;
 }
+// Teste para verificar
+const numeroInformado = parseInt(prompt("Digite um número para verificar se pertence à sequência de Fibonacci:"), 10);
+if (pertenceASequenciaFibonacci(numeroInformado)) {
+    console.log(`O número ${numeroInformado} pertence à sequência de Fibonacci.`);
+} else {
+    console.log(`O número ${numeroInformado} não pertence à sequência de Fibonacci.`);
+}
